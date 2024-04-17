ea406cb38c1a59d86aa7aaf5eebde332acd06074
Tho.Le (Jason Le) /Sotatek - DnU <e.tho.le@wesang.com>
2024-04-17 11:52:21 +0700

    Initial commit

---
+++ src/test/java/com/sotatek/ordermanagement/OrdermanagementApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sotatek.ordermanagement;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermanagementApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
